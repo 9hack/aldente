1676ad1db8276d13c3cba2d3ca9f3d2eeb042080
Joshua Tang <joshua.y.tang@gmail.com>
2017-04-24 23:12:44 -0700

    enabled GL blending for text rendering

--- src/aldente.cpp
+++ src/aldente.cpp
@@ -27,6 +27,8 @@ static void glSetup() {
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
