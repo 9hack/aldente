b9ce888cea38576f4c0cc0f574fe350bdb1f133f
Joshua Tang <joshua.y.tang@gmail.com>
2017-04-29 08:11:22 -0700

    fixed basic shader bug

--- shaders/basic/vert.glsl
+++ shaders/basic/vert.glsl
@@ -17,8 +17,8 @@ uniform mat4 mesh_model;
 void main()
 {
     gl_Position = projection * view * model * mesh_model * vec4(position, 1.0f);
-    frag_pos = vec3(model * mesh_model * (position, 1.0f));
+    frag_pos = vec3(model * mesh_model * vec4(position, 1.0f));
     frag_normal = mat3(transpose(inverse(model * mesh_model))) * normal;
-	frag_pos_light = light_matrix * vec4(frag_pos, 1.0);
-	frag_tex_coord = vec2(tex_coord.x, 1.0 - tex_coord.y); //y-axis usually requires inverting
+    frag_pos_light = light_matrix * vec4(frag_pos, 1.0);
+    frag_tex_coord = vec2(tex_coord.x, 1.0 - tex_coord.y); //y-axis usually requires inverting
 }
