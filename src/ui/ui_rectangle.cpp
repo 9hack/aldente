166003b8b26d5fe4688e839152f4450c350b6345
Joshua Tang <joshua.y.tang@gmail.com>
2017-04-30 23:45:36 -0700

    clarified comment

--- src/ui/ui_rectangle.cpp
+++ src/ui/ui_rectangle.cpp
@@ -6,6 +6,8 @@ void UIRectangle::draw(Render2D &renderer_2d,
     renderer_2d.render_rectP(offset_x + start_x,
                              offset_y + start_y,
                              width, height,
-                             color, 0, alpha); // pass 0 for no texture
+                             color,
+                             0, // 0 is passed to indicate that no texture is bound
+                             alpha);
 }
 
