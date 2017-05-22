6d49bfd70fa8c763fa006fc6bf38344391e378d9
Joshua Tang <joshua.y.tang@gmail.com>
2017-05-21 19:49:35 -0700

    fix comment

--- src/ui/clock_ui.cpp
+++ src/ui/clock_ui.cpp
@@ -6,8 +6,9 @@
 ClockUI::ClockUI(float aspect)
     : clock(40.f * aspect, 90.f, 20.f * aspect, 10.f, Color::WHITE, Color::BLACK) {
 
-    // TEST. REMOVE ME.
     attach(clock);
+
+    // TEST. REMOVE ME.
     Timer::get()->do_every(std::chrono::seconds(1), [&]() {
         static int seconds = 0;
         ++seconds;
