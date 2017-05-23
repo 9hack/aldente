a16df27c647b973e61d9872c41c4f31cb83188e5
Daniel Lee <dml016@ucsd.edu>
2017-05-22 19:23:55 -0700

    Added footsteps

--- src/audio/audio_manager.cpp
+++ src/audio/audio_manager.cpp
@@ -6,6 +6,7 @@ const std::string AudioManager::BUILD_MUSIC = "assets/audio/music/mikoto.wav";
 const std::string AudioManager::DUNGEON_MUSIC = "assets/audio/music/k_theme.wav";
 
 const std::string AudioManager::BUILD_CONFIRM_SOUND = "assets/audio/sound/build_confirm.wav";
+const std::string AudioManager::FOOTSTEPS_SOUND = "assets/audio/sound/footsteps.wav";
 
 AudioManager::AudioManager() : muted(true) {
     loadSounds();
@@ -21,40 +22,21 @@ AudioManager::AudioManager() : muted(true) {
             std::cerr << "AudioManager: Cannot open " << filename << std::endl;;
         }
         music.setLoop(true);
-        if (!muted)
-            std::cerr << "Playing music" << std::endl;
+        if (!muted) {
             music.setVolume(d.volume);
             music.play();
+        }
     });
 
     events::sound_effects_event.connect([&](const events::AudioData &d) {
         std::string filename = d.filename;
-        //if (!muted) {
-            std::cerr << "Start sound effects event." << std::endl;
-            //sf::SoundBuffer soundBuffer;
-            if (!build_confirm_buffer->loadFromFile("assets/audio/music/k_theme.wav")) {
-                std::cerr << "AudioManager: Cannot open" << BUILD_CONFIRM_SOUND << std::endl;;
-            }
-            sf::Sound sound(*build_confirm_buffer);
-            std::cerr << "Sound buffer at " << sound.getBuffer() << " with volume " << sound.getVolume() << std::endl;
-            sound.play();
-            std::cerr << "Status right after play is " << sound.getStatus() << std::endl;
-            std::cerr << "End sound effects event." << std::endl;
-        //}
-
-        /*
-        if (!sound.openFromFile(filename)) {
-            std::cerr << "AudioManager: Cannot open " << filename << std::endl;;
+        if (!muted) {
+            sounds[filename]->setVolume(d.volume);
+            sounds[filename]->play();
         }
-
-        std::cerr << "Playing sound" << std::endl;
-        sound.setVolume(d.volume);
-        sound.play();
-        */
     });
 
     events::toggle_mute_event.connect([&]() {
-        std::cerr << "Toggling mute" << std::endl;
         muted = !muted;
         if (muted) {
             music.pause();
@@ -64,12 +46,22 @@ AudioManager::AudioManager() : muted(true) {
     });
 }
 
+void AudioManager::loadSound(sf::SoundBuffer * soundBuffer, std::string filename) {
+    if (!soundBuffer->loadFromFile(filename)) {
+        std::cerr << "AudioManager: Cannot open" << filename << std::endl;;
+    }
+}
+
 void AudioManager::loadSounds() {
     // Load sound effects into memory
-    build_confirm_buffer = new sf::SoundBuffer();
-    if (!build_confirm_buffer->loadFromFile(BUILD_CONFIRM_SOUND)) {
-        std::cerr << "AudioManager: Cannot open" << BUILD_CONFIRM_SOUND << std::endl;;
-    }
-    build_confirm_sound.setBuffer(*build_confirm_buffer);
-    sounds[BUILD_CONFIRM_SOUND] = build_confirm_sound;
+
+    // BUILD_CONFIRM_SOUND
+    loadSound(&build_confirm_buffer, BUILD_CONFIRM_SOUND);
+    build_confirm_sound.setBuffer(build_confirm_buffer);
+    sounds[BUILD_CONFIRM_SOUND] = &build_confirm_sound;
+
+    // FOOTSTEPS_SOUND
+    loadSound(&footsteps_buffer, FOOTSTEPS_SOUND);
+    footsteps_sound.setBuffer(footsteps_buffer);
+    sounds[FOOTSTEPS_SOUND] = &footsteps_sound;
 }
--- src/audio/audio_manager.h
+++ src/audio/audio_manager.h
@@ -11,8 +11,9 @@ public:
     static const std::string DUNGEON_MUSIC;
 
     static const std::string BUILD_CONFIRM_SOUND;
+    static const std::string FOOTSTEPS_SOUND;
 
-    std::unordered_map<std::string, sf::Sound> sounds;
+    std::unordered_map<std::string, sf::Sound *> sounds;
 
     AudioManager();
 private:
@@ -20,9 +21,12 @@ private:
     bool muted;
     
     // Sound effects
-    sf::SoundBuffer * build_confirm_buffer;
+    sf::SoundBuffer build_confirm_buffer;
     sf::Sound build_confirm_sound;
-    //sf::Music sound;
 
+    sf::SoundBuffer footsteps_buffer;
+    sf::Sound footsteps_sound;
+
+    void loadSound(sf::SoundBuffer * soundBuffer, std::string filename);
     void loadSounds();
 };
