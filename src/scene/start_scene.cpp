457399f1cee8278f54360b5d739c19fc4f054a6f
Joshua Tang <joshua.y.tang@gmail.com>
2017-06-06 06:05:14 -0700

    fix cam rotation

--- src/scene/start_scene.cpp
+++ src/scene/start_scene.cpp
@@ -48,7 +48,7 @@ void StartScene::c_setup() {
 
     // Transition camera when main menu is done.
     events::ui::disable_main_menu.connect([&]() {
-        events::camera_anim_rotate_event(glm::vec3(0, 1, 0), 190, 1500, [](){
+        events::camera_anim_rotate_event(glm::vec3(0, 1, 0), 180, 1500, [](){
             events::camera_anim_position_event(glm::vec3(3.f, 0.f, 6.f), 1500, [](){});
         });
     });
