b01f09bb1264801cb8c44fa7720670dcd0bddd11
Austin Pukasamsombut <austinpuk@gmail.com>
2017-06-08 06:00:00 -0700

    filled out trap info

--- src/game_objects/traps/confuse_trap.cpp
+++ src/game_objects/traps/confuse_trap.cpp
@@ -1,8 +1,8 @@
-#include "slow_trap.h"
+#include "confuse_trap.h"
 #include "asset_loader.h"
 
-SlowTrap::SlowTrap(int x, int z, int id) : Trap(x, z, id) {
-    tag = "SLOW_TRAP";
+ConfuseTrap::ConfuseTrap(int x, int z, int id) : Trap(x, z, id) {
+    tag = "CONFUSE_TRAP";
 
     if (id == ON_SERVER) {
         // Creates Rigid Body
@@ -18,7 +18,7 @@ SlowTrap::SlowTrap(int x, int z, int id) : Trap(x, z, id) {
     }
 }
 
-void SlowTrap::s_on_collision(GameObject *other) {
+void ConfuseTrap::s_on_collision(GameObject *other) {
     Player *player = dynamic_cast<Player*>(other);
     if (player) {
         player->s_slow();
@@ -28,13 +28,13 @@ void SlowTrap::s_on_collision(GameObject *other) {
     }
 }
 
-void SlowTrap::c_on_collision(GameObject *other) {
+void ConfuseTrap::c_on_collision(GameObject *other) {
     Player* player = dynamic_cast<Player*>(other);
     if (player)
         player->c_slow();
 }
 
-void SlowTrap::setup_model() {
-    attach_model(AssetLoader::get_model("water"));
+void ConfuseTrap::setup_model() {
+    attach_model(AssetLoader::get_model("confusion"));
     set_scale({ 0.004f, 0.004f, 0.004 });
 }
--- src/game_objects/traps/confuse_trap.h
+++ src/game_objects/traps/confuse_trap.h
@@ -5,14 +5,14 @@
 /*
     A tile-based trap. Freezes the player, and then slowly regains their mvoespeeed
 */
-class SlowTrap : public Trap {
+class ConfuseTrap : public Trap {
 public:
-    SlowTrap(int x, int z, int id = 0);
+    ConfuseTrap(int x, int z, int id = 0);
 
     void s_on_collision(GameObject *other) override;
     void c_on_collision(GameObject * other) override;
 
     void setup_model() override;
 private:
-    btBoxShape *hit_box = new btBoxShape(btVector3(0.25f, 0.25f, 0.25f));
+    btBoxShape *hit_box = new btBoxShape(btVector3(0.5f, 0.5f, 0.5f));
 };
