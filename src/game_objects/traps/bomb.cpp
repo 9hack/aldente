8071178febf0e37285fae7a8bdb34fc8507cbc0f
Austin Pukasamsombut <austinpuk@gmail.com>
2017-06-08 08:42:55 -0700

    adding bomb logic

--- src/game_objects/traps/bomb.cpp
+++ src/game_objects/traps/bomb.cpp
@@ -1,10 +1,9 @@
-#include "spikes.h"
+#include "bomb.h"
 #include "asset_loader.h"
+#include "game/game_state.h"
 
-/************SPIKES***************/
-
-Spikes::Spikes(int x, int z, int id) : CollisionTrap(x, z, id) {
-    tag = "SPIKES";
+Bomb::Bomb(int x, int z, int id) : Trap(x, z, id) {
+    tag = "BOMB";
 
     if (id == ON_SERVER) {
         //Creates Rigid Body
@@ -14,14 +13,80 @@ Spikes::Spikes(int x, int z, int id) : CollisionTrap(x, z, id) {
         rigid.is_ghost = true;
         rigid.position = { x, 0.0f, z };
         events::add_rigidbody_event(rigid);
+
+        blast_radius = 4.0f;
     }
 }
 
-void Spikes::play_trigger_animation() {
-    anim_player.play_if_paused("trigger");
+void Bomb::s_on_collision(GameObject *other) {
+
+    // Can't explode twice
+    if (exploded)
+        return;
+
+    // Explode if stepped on by a player
+    Player *player = dynamic_cast<Player*>(other);
+    if (player) {
+        s_explode();
+        events::dungeon::network_collision_event(player->get_id(), id);
+        exploded = true;
+    }
 }
 
-void Spikes::setup_model() {
-    attach_model(AssetLoader::get_model("spikes"));
-    set_scale({ 0.5f, 2.0f, 0.5f });
+void Bomb::c_on_collision(GameObject *other) {
+    if (!exploded) {
+        c_explode();
+        exploded = true;
+    }
+
+    Player* player = dynamic_cast<Player*>(other);
+    if (player)
+        player->c_take_damage();
+}
+
+void Bomb::s_reset() {
+
+}
+
+void Bomb::c_reset() {
+
+}
+
+void Bomb::setup_model() {
+    // Setup initially as mine
+    attach_model(AssetLoader::get_model("mine"));
+    set_scale({ 0.005f, 0.005f, 0.005f });
+}
+
+// Damages all player within blast radius
+void Bomb::s_explode() {
+    for (auto pair : GameState::players) {
+        Player *player = pair.second;
+        if (player) {
+            if (glm::distance(player->transform.get_position(), transform.get_position()) <= blast_radius) {
+                player->s_take_damage();
+                // Send signal to client that this player was hit
+                events::dungeon::network_collision_event(player->get_id(), id);
+            }
+        }
+    }
+
+    // Disables after animation ends
+    Timer::get()->do_after(std::chrono::milliseconds(1000), 
+        [&]() {
+        disable();
+    });
+}
+
+// Switch to explosion animation
+void Bomb::c_explode() {
+    attach_model(AssetLoader::get_model("explosion"));
+    set_scale({ 0.005f, 0.005f, 0.005f });
+
+    // Make Glow and Transparent
+    model->multiply_colors({ 2.0f, 2.0f, 2.0f, false });
+    model->set_alpha(0.8f);
+    
+    anim_player.set_anim("go", 1.0f, false);
+    anim_player.play();
 }
--- src/game_objects/traps/bomb.h
+++ src/game_objects/traps/bomb.h
@@ -1,13 +1,24 @@
-#include "collision_trap.h"
+#pragma once
 
-class Spikes : public CollisionTrap {
+#include "trap.h"
+
+class Bomb : public Trap {
 public:
-    Spikes(int x, int z, int id = 0);
+    Bomb(int x, int z, int id = 0);
+
+    void s_on_collision(GameObject *other) override;
+    void c_on_collision(GameObject *other) override;
+
+    void s_reset() override;
+    void c_reset() override;
 
     void setup_model() override;
-
-    void play_trigger_animation() override;
-
 private:
+    float blast_radius;
+    bool exploded;
+
     btBoxShape *hit_box = new btBoxShape(btVector3(0.25f, 0.25f, 0.25f));
+
+    void s_explode();
+    void c_explode();
 };
