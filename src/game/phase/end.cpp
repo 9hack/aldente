8602725daac7a723a5bd3db0422c5f1ae9a54ab2
Richard G Lin <richwierdo@gmail.com>
2017-06-09 08:09:45 -0700

    Preliminary end phase

--- src/game/phase/end.cpp
+++ src/game/phase/end.cpp
@@ -4,13 +4,34 @@ EndPhase::EndPhase(Context &context)
     : Phase(context) {}
 
 void EndPhase::s_setup() {
+    std::cerr << "end s setup\n";
     next = s_phase_when_done();
+
+    // Set the scene
+    // GameState::set_scene(&GameState::end_scene);
 }
 
 void EndPhase::s_teardown() {
 }
 
 void EndPhase::c_setup() {
+    std::cerr << "end c setup\n";
+
+    // Set the scene
+    // GameState::set_scene(&GameState::end_scene);
+
+    int high_score = 0;
+    Player* winner = nullptr;
+    for (int player_id : context.player_ids) {
+        Player* player = dynamic_cast<Player*>(GameObject::game_objects[player_id]);
+        if (!winner || player->get_coins() > high_score) {
+            winner = player;
+            high_score = player->get_coins();
+        }
+    }
+
+    // TODO: use the winner player (and their gold amount if you want)
+    std::cerr << "winner player: " << winner->get_id() << " with gold: " << high_score << "\n";
 }
 
 void EndPhase::c_update() {
