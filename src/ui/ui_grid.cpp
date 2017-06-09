5a952edc0da0e9a8d7ee89356df57acf1351dd9a
Daniel M Lee <dml016@UCSD.EDU>
2017-06-08 17:00:34 -0700

    Integrated select1 sounds to character selection and trap selection. Edited select so that it's more subtle

--- src/ui/ui_grid.cpp
+++ src/ui/ui_grid.cpp
@@ -111,6 +111,9 @@ void UIGrid::toggle_current_selection_halo() {
 
 void UIGrid::move_selection(Direction d) {
     toggle_current_selection_halo();
+	int old_selection_row = selection_row;
+	int old_selection_col = selection_col;
+
     switch (d) {
         case Direction::UP:
             selection_row = glm::max(0, selection_row-1);
@@ -127,5 +130,10 @@ void UIGrid::move_selection(Direction d) {
         default:
             break;
     }
+
+	if ((selection_row != old_selection_row) || (selection_col != old_selection_col)) {
+		events::sound_effects_event(events::AudioData(AudioManager::SELECT1_SOUND, false));
+	}
+
     toggle_current_selection_halo();
 }
